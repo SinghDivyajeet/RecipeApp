7e9cf2d1353221bae9124ea790a9123c39660fb2
SinghDivyajeet <46.divyajeetsingh@gmail.com>
2020-01-08 17:40:09 +0530

    added recipe list

--- src/app/recipies/recipe-list/recipe-list.component.ts
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -7,7 +7,8 @@ import { Recipie } from '../recipies.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes:Recipie[] = [new Recipie('test recipie',"This is a test recipe","https://cdn.apartmenttherapy.info/image/upload/v1567541461/k/Photo/Recipes/2019-09-how-to-shrimp-alfredo/HT-Shrimp-Alfredo_103.jpg")];
+  recipe = new Recipie('test recipie',"This is a test recipe","https://cdn.apartmenttherapy.info/image/upload/v1567541461/k/Photo/Recipes/2019-09-how-to-shrimp-alfredo/HT-Shrimp-Alfredo_103.jpg");
+  recipes:Recipie[] = [this.recipe,this.recipe,this.recipe,this.recipe,];
   constructor() { }
 
   ngOnInit() {
